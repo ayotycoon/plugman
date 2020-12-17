9c5e339715aca61e8777d938d012d8c0e78a1ea0
ayotycoon <kaysehventeen@gmail.com>
2020-12-17 20:10:10 +0000

    Deploying to build from  @ 315fb3ffd96f5b5e25d035706d3130f0e5dd0eaa 🚀

--- precache-manifest.8f94b365b547444404232464b225f1b6.js
+++ precache-manifest.8f94b365b547444404232464b225f1b6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8912e0e1f777900ea260edc162f53443",
+    "revision": "f2db2619149f10f945a0aaeb5fe439b3",
     "url": "/index.html"
   },
   {
-    "revision": "648c4139c804d0639374",
+    "revision": "7e0e28e19526761b1c8d",
     "url": "/static/css/main.df14ee98.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.1e825efd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "648c4139c804d0639374",
-    "url": "/static/js/main.76047f5b.chunk.js"
+    "revision": "7e0e28e19526761b1c8d",
+    "url": "/static/js/main.f60b8904.chunk.js"
   },
   {
     "revision": "0d026d56eabe46656a93",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.03512e3d9820049059e7a9ed38353437.js"
+  "/precache-manifest.8f94b365b547444404232464b225f1b6.js"
 );
 
 self.addEventListener('message', (event) => {
